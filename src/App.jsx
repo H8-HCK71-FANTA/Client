825bec2af587ed1a3a8be189b98cab2689629890
joufando <joufando@gmail.com>
2024-06-20 10:43:33 +0700

    add cowboy image

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import SingleCard from "./components/SingleCard";
 import { useSocket } from "./hooks/useSocket";
 
 const cardImages = [
-  { src: "/img/helmet-1.png", matched: false },
-  { src: "/img/potion-1.png", matched: false },
-  { src: "/img/ring-1.png", matched: false },
-  { src: "/img/scroll-1.png", matched: false },
-  { src: "/img/shield-1.png", matched: false },
-  { src: "/img/sword-1.png", matched: false },
+  { src: "/img/cat.jpg", matched: false },
+  { src: "/img/cowboy.webp", matched: false },
+  { src: "/img/gorilla.jpg", matched: false },
+  { src: "/img/owl.jpg", matched: false },
+  { src: "/img/parrot.jpg", matched: false },
+  { src: "/img/tiger.jpg", matched: false },
 ];
 
 export default function App() {
