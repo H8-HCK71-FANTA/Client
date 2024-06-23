b4f26b65975a503cee020f86d233f4964d7921cb
bernadetarp <bernadetargn@gmail.com>
2024-06-24 00:29:27 +0700

    feat: add start page and lobby

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,6 +1,13 @@
 import "../components/SIngleCard.css";
+import PropTypes from "prop-types";
 
-export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+export default function SingleCard({
+  card,
+  handleChoice,
+  flipped,
+  disabled,
+  test,
+}) {
   const handleClick = () => {
     if (!disabled) {
       handleChoice(card);
@@ -8,16 +15,24 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
   };
 
   return (
-    <div className="card">
+    <div className="card" key={test}>
       <div className={flipped ? "flipped" : ""}>
-        <img className="front" src={card.src} alt="card-front" />
+        <img className="front h-full object-cover" src={card.src} alt="card front" />
         <img
-          className="back"
-          src="/img/cover.png"
+          className="back h-full object-cover"
+          src="/img/background.jpg"
           onClick={handleClick}
           alt="card back"
         />
       </div>
     </div>
   );
+}
+
+SingleCard.propTypes = {
+  card: PropTypes.any,
+  handleChoice: PropTypes.any,
+  flipped: PropTypes.any,
+  disabled: PropTypes.any,
+  test: PropTypes.any
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,9 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // ...
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
-  plugins: [
-    // ...
-    flowbite.plugin(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite.plugin()],
 };
